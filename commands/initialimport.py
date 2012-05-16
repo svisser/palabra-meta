944193c5ce7b1d03cec48977495422af9235a8f4
Simeon Visser <simeon87@gmail.com>
2012-05-16 22:34:36 +0200

    Updated initial import command for inserting n crosswords

--- commands/initialimport.py
+++ commands/initialimport.py
@@ -2,18 +2,29 @@ import constants
 from database import connection, db
 
 
-def run(*args, **kwargs):
+def create_crossword(i):
+    s_i = str(i)
+    return {
+        constants.META_TITLE: "Author " + s_i,
+        constants.META_CREATOR: "Creator " + s_i,
+        constants.META_CONTRIBUTOR: "Contributor " + s_i,
+        constants.META_RIGHTS: "Copyright " + s_i,
+        constants.META_PUBLISHER: "Publisher " + s_i,
+        constants.META_DATE: "Date " + s_i,
+    }
+
+
+def run(args):
     result = raw_input("""This will delete all content in the database. Do you wish to continue (yes/no)? """)
     if result != 'yes':
         print "No actions were performed. Exiting."
         return
+    assert len(args) == 2, "Initial import got an unexpected number of arguments."
+    try:
+        n_crosswords = int(args[1])
+    except ValueError:
+        n_crosswords = None
+    assert n_crosswords is not None, "The number of initial crosswords could not be determined."
     connection.drop_database('palabra')
-    p1 = {
-        constants.META_TITLE: "Author 1",
-        constants.META_CREATOR: "John Johnsson",
-        constants.META_CONTRIBUTOR: "Contributor",
-        constants.META_RIGHTS: "Copyright notice",
-        constants.META_PUBLISHER: "The publisher",
-        constants.META_DATE: "The date",
-    }
-    db.crosswords.insert(p1)
+    db.crosswords.insert([create_crossword(i) for i in xrange(n_crosswords)])
+    print "Total crosswords inserted:", str(n_crosswords)
